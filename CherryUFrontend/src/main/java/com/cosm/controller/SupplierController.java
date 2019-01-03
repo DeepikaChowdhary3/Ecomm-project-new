2b3ce77f2a9a21f8349b76d120b8b595723536b5
DeepikaChowdhary3 <45058709+DeepikaChowdhary3@users.noreply.github.com>
2019-01-03 20:08:58 +0530

    Add files via upload

--- CherryUFrontend/src/main/java/com/cosm/controller/SupplierController.java
+++ CherryUFrontend/src/main/java/com/cosm/controller/SupplierController.java
@@ -3,9 +3,10 @@ package com.cosm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +17,7 @@ import com.cosm.dao.SupplierDAO;
 import com.cosm.model.Supplier;
 
 @Controller
+
 public class SupplierController {
 
 	@Autowired
